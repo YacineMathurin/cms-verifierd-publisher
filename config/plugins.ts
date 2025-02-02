21b1cd81bb83661e99c274c7c02c4d2382cb21cc
Yacine <yacinemathurin@gmail.com>
2025-02-02 23:56:11 +0100

    feat: bouton to download pdf

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,28 @@
-export default () => ({});
+// export default () => ({});
+
+
+// ./config/plugins.ts`
+export default () => ({
+    'preview-button': {
+      config: {
+        contentTypes: [
+          {
+            uid: "api::article.article",
+            // draft: {
+            //   url: 'http://localhost:3000/api/preview',
+            //   query: {
+            //     type: 'page',
+            //   },
+            //   openTarget: 'StrapiPreviewPage',
+            // },
+            published: {
+              url: 'http://localhost:3000/',
+              openTarget: 'StrapiPage',
+            },
+          },
+        
+        ],
+      },
+    },
+  });
+
