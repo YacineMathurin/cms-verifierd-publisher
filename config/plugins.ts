fa1bcd066a7817f9706fcbfdada342f1e66fe26c
Yacine <yacinemathurin@gmail.com>
2025-06-22 05:53:36 +0200

    feat: upd plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,16 @@
-// export default () => ({});
-
-
-// ./config/plugins.ts`
+// ./config/plugins.ts
 export default () => ({
-    'preview-button': {
-      config: {
-        contentTypes: [
-          {
-            uid: "api::parcelle.parcelle",
-           
-            published: {
-              url: 'http://localhost:3000/api/generate-pdf?id={id}&Id_Parcelle={Id_Parcelle}&Lotissement={Lotissement}&Ilot={Ilot}&Parcelle={Parcelle}&Nom={Nom}&Prenom={Prenom}&Superficie=${Superficie}&createdAt={createdAt}&updatedAt={updatedAt}&publishedAt={publishedAt}&Agent_GPS={Agent_GPS}&Litige={Litige}',  
-              openTarget: 'StrapiPage',
-            },
+  'preview-button': {
+    config: {
+      contentTypes: [
+        {
+          uid: "api::parcelle.parcelle",
+          published: {
+            url: 'http://localhost:3000/api/generate-pdf?id={id}&Id_Parcelle={Id_Parcelle}&Lotissement={Lotissement}&Ilot={Ilot}&Parcelle={Parcelle}&Nom={Nom}&Prenom={Prenom}&Superficie={Superficie}&createdAt={createdAt}&updatedAt={updatedAt}&publishedAt={publishedAt}&Litige={Litige}',
+            openTarget: 'StrapiPage',
           },
-        
-        ],
-      },
+        },
+      ],
     },
-  });
-
+  },
+});
